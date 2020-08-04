93e475489cd6b34a697f5da451abe56d17f12129
sansilvamilena <sansilvamilena@gmail.com>
2020-08-03 22:56:45 -0300

    First Commit

---
+++ src/main/java/com/mycompany/consultorio/Medico.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.consultorio;
+
+/**
+ *
+ * @author milena
+ */
+public class Medico {
+    
+}
